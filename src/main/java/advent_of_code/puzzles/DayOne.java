e8bec110f04b5a28eb5c32e7812d9a4212b51de0
jmhossler <john.m.hossler@gmail.com>
2020-12-01 22:48:32 -0800

    Completed day 1 of AOC 2020

--- src/main/java/advent_of_code/puzzles/DayOne.java
+++ src/main/java/advent_of_code/puzzles/DayOne.java
@@ -2,9 +2,16 @@ package advent_of_code.puzzles;
 
 import advent_of_code.InputProvider;
 import advent_of_code.Puzzle;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import lombok.Builder;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Builder
 public class DayOne implements Puzzle {
@@ -12,18 +19,42 @@ public class DayOne implements Puzzle {
 
     @Override
     public Integer partOne() throws IOException {
-        String input = inputProvider.getInput();
+        Integer expectedSum = 2020;
+        Map<Integer, Integer> neededValueMap = new HashMap();
+        List<Integer> expenseReport = getExpenseReport();
+
+        for(Integer expense: expenseReport) {
+            if (neededValueMap.containsKey(expense)) {
+                return neededValueMap.get(expense) * expense;
+            }
+            neededValueMap.put(expectedSum - expense, expense);
+        }
         return null;
     }
 
     @Override
     public Integer partTwo() throws IOException {
-        String input = inputProvider.getInput();
+        Integer expectedSum = 2020;
+        List<Integer> expenseReport = getExpenseReport();
+        List<List<Integer>> possibleCombinations = Lists.cartesianProduct(expenseReport, expenseReport, expenseReport);
+
+        for (List<Integer> possibleCombination : possibleCombinations) {
+            if (possibleCombination.stream().mapToInt(Integer::intValue).sum() == expectedSum.intValue()) {
+                return possibleCombination.stream().reduce(1, (a,b) -> a * b);
+            }
+        }
         return null;
     }
 
     @Override
     public Integer getDay() {
         return 1;
+    }
+
+    private List<Integer> getExpenseReport() throws IOException {
+        return Arrays.stream(inputProvider.getInput().split("\\n"))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
